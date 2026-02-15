af758b2187a32d0c981976a7b08e5a1bcf4564aa
Ragu <111031929+Raghulands@users.noreply.github.com>
2026-02-15 10:25:35 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,2 +1,13 @@
-public class LearYaer {
+import java.util.*;
+public class LeapYear {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
 }
